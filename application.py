3387064527d9d3d4c3e1c877bd29629cdb23bdf5
miklehook <36674994+miklehook@users.noreply.github.com>
2019-05-03 02:24:27 +0300

    bug fixed

--- application.py
+++ application.py
@@ -1,26 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
-from application import models
+#from application import db
+#from application.models import Data
+
 
 app = Flask(__name__)
 app.debug=True
 # change this to your own value
 app.secret_key = 'cC1YCIWOj9GgWspgNEo2'
+
 @app.route('/')
 @app.route('/sberbank_op.html')
 def sberbank_op():
 	return render_template('sberbank_op.html')
-@app.route('/sberbank_reg.html',method=['POST'])
+@app.route('/sberbank_reg.html',methods=['POST'])
 def sberbank_reg():
-    inn = request.form['inn']
-    return '<h1> INN{}</>'.format(inn)
-
+    return render_template('sberbank_reg.html')
 @app.route('/sberbank_doc.html')
 def sberbank_doc():
-    
-    
-    
-    #post = Data(inn = inn)
-    
     return render_template('sberbank_doc.html')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
